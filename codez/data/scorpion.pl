84daffe7aea98685d83ba2a77fe85cc5fe8d0b10
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2022-05-03 12:47:02 +0300

    Cleaned up vestigial clauses from scorpion, spider classes.

--- codez/data/scorpion.pl
+++ codez/data/scorpion.pl
@@ -9,11 +9,6 @@ scorpion:attribute(charms,30).
 scorpion:attribute(ken,30).
 scorpion:attribute(passions,60).
 
-scorpion:condition(init,0).
-scorpion:condition(tr,0).
-scorpion:condition(sr,0).
-scorpion:condition(wounds,0).
-
 scorpion:base_attack([name-'Pincer Attack'
                      ,keywords-['Crushing','Shredding']
                      ,damage-'1 Wound'
@@ -36,35 +31,3 @@ scorpion:special_attack([name-'Stinger Attack'
                         ,miss_1-'Attacker Recoils'
                         ,miss_2-['(Alt)','']]
                        ).
-
-
-scorpion:survival(hunger,0).
-scorpion:survival(luck,[11]).
-
-scorpion:specific_ability(carapace,0).
-scorpion:specific_ability(flying,0).
-scorpion:specific_ability(swarming,0).
-scorpion:specific_ability(venomous,0).
-scorpion:specific_ability(web,0).
-
-scorpion:common_ability(construction,0).
-scorpion:common_ability(eusociology,0).
-scorpion:common_ability(exploration,0).
-scorpion:common_ability(foraging,0).
-scorpion:common_ability(healing,0).
-scorpion:common_ability(hunting,0).
-scorpion:common_ability(leadership,0).
-scorpion:common_ability(perception,0).
-scorpion:common_ability(signalling,0).
-scorpion:common_ability(sneaking,0).
-
-scorpion:effect(agony,0).
-scorpion:effect(bleeding,0).
-scorpion:effect(blind,0).
-scorpion:effect(charmed,0).
-scorpion:effect(confused,0).
-scorpion:effect(immobilised,0).
-scorpion:effect(infected,0).
-scorpion:effect(paralysed,0).
-scorpion:effect(poisoned,0).
-scorpion:effect(stunned,0).
--- codez/data/spider.pl
+++ codez/data/spider.pl
@@ -9,11 +9,6 @@ spider:attribute(charms,30).
 spider:attribute(ken,45).
 spider:attribute(passions,30).
 
-spider:condition(init,0).
-spider:condition(tr,0).
-spider:condition(sr,0).
-spider:condition(wounds,0).
-
 spider:base_attack([name-'Web Attack'
                      ,keywords-['Entangling','Healing']
                      ,damage-'N/A'
@@ -36,37 +31,3 @@ spider:special_attack([name-'Bite Attack'
                         ,miss_1-'Attacker Recoils'
                         ,miss_2-['(Alt)','']]
                        ).
-
-
-spider:survival(hunger,0).
-spider:survival(luck,[11]).
-
-spider:specific_ability(carapace,0).
-spider:specific_ability(flying,0).
-spider:specific_ability(swarming,0).
-spider:specific_ability(venomous,0).
-spider:specific_ability(web,0).
-
-spider:common_ability(construction,0).
-spider:common_ability(eusociology,0).
-spider:common_ability(exploration,0).
-spider:common_ability(foraging,0).
-spider:common_ability(healing,0).
-spider:common_ability(hunting,0).
-spider:common_ability(leadership,0).
-spider:common_ability(perception,0).
-spider:common_ability(signalling,0).
-spider:common_ability(sneaking,0).
-
-spider:effect(agony,0).
-spider:effect(bleeding,0).
-spider:effect(blind,0).
-spider:effect(charmed,0).
-spider:effect(confused,0).
-spider:effect(immobilised,0).
-spider:effect(infected,0).
-spider:effect(paralysed,0).
-spider:effect(poisoned,0).
-spider:effect(stunned,0).
-
-
